4ad1409f0f2d75afb1195549c2f0c992bc466297
a14ehsr <a14.ehsr@g.chuo-u.ac.jp>
2019-05-20 14:33:18 +0900

    ビジュアライザ仮実装

---
+++ java/src/ac/a14ehsr/platform/visualizer/Visualizer.java
@@ -0,0 +1,39 @@
+package ac.a14ehsr.platform.visualizer;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Visualizer {
+    private JFrame frame;
+    private JPanel mainPanel;
+
+    private JPanel[][] panels;
+
+    public Visualizer(int width, int height) {
+        frame = new JFrame();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(heigth + 2,width + 2));
+
+        
+        panels = new JPanel[height + 2][width + 2];
+        for(int y = 0; y < panels.length; y++) {
+            for(int x = 0; x < panels[y].length; x++) {
+                panels[y][x] = new JPanel();
+                panels[y][x].setBackground(Color.LIGHT_GRAY);
+                mainPanel.add(panels[y][x]);
+            }
+        }
+
+        frame.getContentPane().add(mainPanel,Layout.CENTER);
+
+
+    }
+}
